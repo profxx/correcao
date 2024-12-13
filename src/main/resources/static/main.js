2816e5194f42cf7d54aa35efe75db3e3f247abce
Alexandre De Carvalho Pimentel <apimentel@firjan.com.br>
2024-12-13 16:47:20 -0300

    JS

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,21 @@
+function convertCursoToLi(curso){
+    return `
+    <li>
+        <h2>${curso.nome}</h2>
+        <div class="info">
+            <div class="textos">
+                <h3>${curso.categoria}</h3>
+                <p>${curso.carga}</p>
+            </div>
+            <button type="button" class="btn btn-primary">Excluir</button>
+        </div>
+    </li>
+    `
+}
+function cadastrar(){}
+function listar(){
+let lista = document.getElementById("lista");
+fetch("/cursos")
+    .then((response) => response.json())
+    .then((jsonresponse) => lista.innerHTML = jsonresponse.map(convertCursoToLi).join(""))
+}
